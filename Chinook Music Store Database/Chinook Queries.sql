194495401d26e75aca92097244e6c638ef2f1d8d
LReyna <leo.reyna@gmail.com>
2023-12-16 15:09:53 -0500

    Updating Files

--- Chinook Music Store Database/Chinook Queries.sql
+++ Chinook Music Store Database/Chinook Queries.sql
@@ -12,17 +12,13 @@ SELECT
 FROM dbo.Customer
 WHERE Country <> 'USA'
 
-/* 
-Create a query showing a unique list of billing countries 
-from the Invoice table
-*/
+-- Create a query showing a unique list of billing countries from the Invoice table
 
 SELECT DISTINCT BillingCountry
 FROM dbo.Invoice
 
-/*
-Who are the top 5 artists to produce most albums?
-*/
+-- Who are the top 5 artists to produce most albums?
+
 SELECT 
 TOP 5 ART.Name AS ArtistName, 
 COUNT(ALB.AlbumId) AS AlbumCount
@@ -32,10 +28,7 @@ LEFT JOIN dbo.Artist AS ART
 GROUP BY ART.Name
 ORDER BY AlbumCount DESC;
 
-/*
-Number of tracks sold per genre
-*/
-
+-- Number of tracks sold per genre
 
 SELECT  
     gr.Name as [Genre Name], 
@@ -48,7 +41,7 @@ LEFT JOIN dbo.Genre as gr
 GROUP by gr.Name
 ORDER BY [Num of Tracks Sold] DESC;
 
-/* Rock Sales by Year */
+-- Rock Sales by Year
 
 SELECT 
     YEAR(inv.InvoiceDate) as YearDate,
@@ -65,7 +58,7 @@ WHERE grt.Name like 'Rock'
 GROUP BY YEAR(InvoiceDate), grt.Name
 ORDER BY YEAR(InvoiceDate) DESC;
 
-/* Top 3 cities */
+-- What are the top 3 cities
 
 WITH CitySales AS (
     SELECT
@@ -82,3 +75,20 @@ SELECT
     FORMAT(TotalSales, 'C', 'EN-US') AS TotalSales$
 FROM CitySales
 WHERE RowNum <= 3;
+
+-- Top 5 billing countries
+
+WITH CountrySalesTop5 as (
+SELECT inv.BillingCountry,
+    SUM(inv.Total) AS TotalSales,
+    ROW_NUMBER() OVER (ORDER BY SUM(Total) DESC) AS RowNum
+FROM dbo.Invoice as inv
+LEFT JOIN dbo.InvoiceLine as inl 
+    ON inv.InvoiceId = inl.InvoiceId 
+GROUP BY inv.BillingCountry
+)
+SELECT 
+    BillingCountry,
+    FORMAT(TotalSales, 'c', 'en-us') AS Totals$
+    FROM CountrySalesTop5
+    WHERE RowNum <= 5
