8acf1f8dc2ca6ca7cb180a1c30da9a5a42f0f0a6
LReyna <leo.reyna@gmail.com>
2025-05-27 21:10:50 -0400

    updating files section 4

--- SQL for Data Analysis Advanced SQL Querying Techniques/Section 5 - Subqueries & CTEs.sql
+++ SQL for Data Analysis Advanced SQL Querying Techniques/Section 5 - Subqueries & CTEs.sql
@@ -46,3 +46,52 @@ LEFT JOIN
         GROUP BY country) AS country_hs
 ON hs.country = country_hs.country
 WHERE hs.country = 'United States'  
+
+-- Using Multiple Subqueries
+
+-- this query stacks the historical and current happiness scores for each country
+SELECT  hs.year, 
+        hs.country, 
+        hs.happiness_score,
+        country_hs.avg_hs_by_country
+FROM
+        (SELECT year, country, happiness_score  
+        FROM happiness_scores
+        UNION ALL
+        SELECT 2024, country, ladder_score 
+        FROM happiness_scores_current) as hs
+LEFT JOIN
+        (SELECT country, AVG(happiness_score) AS avg_hs_by_country
+        FROM happiness_scores
+        GROUP BY country) AS country_hs
+ON hs.country = country_hs.country;
+
+-- Nested Subqueries
+-- this query stacks the historical and current happiness scores for each country
+SELECT *
+FROM (
+    SELECT  hs.year, 
+            hs.country, 
+            hs.happiness_score,
+            country_hs.avg_hs_by_country
+    FROM
+            (SELECT year, country, happiness_score  
+            FROM happiness_scores
+            UNION ALL
+            SELECT 2024, country, ladder_score 
+            FROM happiness_scores_current) as hs
+    LEFT JOIN
+            (SELECT country, AVG(happiness_score) AS avg_hs_by_country
+            FROM happiness_scores
+            GROUP BY country) AS country_hs
+    ON hs.country = country_hs.country
+) AS hs_country_hs
+WHERE happiness_score > avg_hs_by_country + 1.0;
+
+-- 3. Multiple Subqueries
+-- Return happiness scores for 2015 - 2024 
+SELECT DISTINCT year FROM happiness_scores; -- data from 2015-2023
+SELECT * FROM happiness_scores_current; -- data from 2024
+
+SELECT year, country, happiness_score
+FROM happiness_scores AS hs
