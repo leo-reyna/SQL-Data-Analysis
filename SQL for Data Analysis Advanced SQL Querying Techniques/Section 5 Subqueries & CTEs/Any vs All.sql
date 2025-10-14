d713d2f4b6792cb8ed865578c8ca259689c773ff
LReyna <leo.reyna@gmail.com>
2025-10-13 21:35:05 -0400

    Updating files

---
+++ SQL for Data Analysis Advanced SQL Querying Techniques/Section 5 Subqueries & CTEs/Any vs All.sql
@@ -0,0 +1,13 @@
+USE maven_advanced_sql;
+
+-- SCORES THAT ARE GREATER THAN ANY OF THE CURRENT OF ANY 2024 SCORES
+SELECT *
+FROM    happiness_scores
+WHERE   happiness_score > 
+        ANY(select ladder_score from happiness_scores_current);
+
+-- SCORES THAT ARE GREATER THAN ALL 2024 SCORES
+SELECT *
+FROM    happiness_scores
+WHERE   happiness_score > 
+        ALL(select ladder_score from happiness_scores_current);
