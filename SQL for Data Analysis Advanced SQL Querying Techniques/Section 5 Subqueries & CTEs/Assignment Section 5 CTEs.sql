a007f66696d79c805f06ff1363992e992d48bd0f
LReyna <leo.reyna@gmail.com>
2025-10-18 12:25:06 -0400

    Updating files

---
+++ SQL for Data Analysis Advanced SQL Querying Techniques/Section 5 Subqueries & CTEs/Assignment Section 5 CTEs.sql
@@ -0,0 +1,27 @@
+-- ASSIGNMENT:
+-- Locating total amount spent that is over 200 dollars
+-- only order_id and total_amt_spent columns should be in the final table
+SELECT * FROM orders;
+SELECT * FROM products;
+
+SELECT o.order_id, 
+       SUM(o.units * p.unit_price) as total_amt_spent
+FROM orders as o
+LEFT JOIN products as p     
+       ON o.product_id = p.product_id
+       GROUP BY order_id
+       HAVING total_amt_spent > 200
+       ORDER BY total_amt_spent DESC;
+
+-- Return the number of orders over 200 dollars
+With cte as(SELECT o.order_id, 
+       SUM(o.units * p.unit_price) as total_amt_spent
+       FROM orders as o
+       LEFT JOIN products as p     
+       ON o.product_id = p.product_id
+       GROUP BY order_id
+       HAVING total_amt_spent > 200
+       ORDER BY total_amt_spent DESC
+       )
+SELECT COUNT(*)
+FROM cte;
