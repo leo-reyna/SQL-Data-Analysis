44ce38233b408076a3a04dd380d6f4867f2512d4
LReyna <leo.reyna@gmail.com>
2025-11-02 20:33:41 -0500

    updating files sum more

--- SQL for Data Analysis Advanced SQL Querying Techniques/Section 6 Window Functions/Assignment S6 Window Functions.sql
+++ SQL for Data Analysis Advanced SQL Querying Techniques/Section 6 Window Functions/Assignment S6 Window Functions.sql
@@ -48,3 +48,79 @@ SELECT
 FROM    orders
 WHERE order_id LIKE '%44262'
 ORDER BY order_id, prod_rank 
+
+-- Find the 2nd most popular product with each order
+-- order_id / product_id / units
+
+SELECT * FROM  orders;
+
+-- view the ranking from last assignment
+
+SELECT
+        order_id,
+        product_id,
+        units,
+        DENSE_RANK() OVER(PARTITION BY order_id ORDER BY units DESC) AS prod_rank
+FROM    orders       
+ORDER BY order_id, prod_rank;
+
+-- Add a column that contains the 2nd most popular product
+SELECT
+        order_id,
+        product_id,
+        units,
+        NTH_VALUE(product_id, 2) OVER(PARTITION BY order_id ORDER BY units DESC) AS second_prod
+FROM    orders       
+ORDER BY order_id, second_prod;
+
+-- Creating a CTE to get the 2nd most popular item
+WITH    CTE1 AS 
+(
+        SELECT
+                order_id,
+                product_id,
+                units,
+                NTH_VALUE(product_id, 2) OVER(PARTITION BY order_id ORDER BY units DESC) AS second_prod
+        FROM    orders   
+        ORDER BY order_id, second_prod
+)
+SELECT *  
+FROM    CTE1
+WHERE   product_id = second_prod
+
+-- USING DENSE RANK
+SELECT
+        order_id,
+        product_id,
+        units,
+        DENSE_RANK() OVER(PARTITION BY order_id ORDER BY units DESC) AS prod_rank
+FROM    orders       
+ORDER BY order_id, prod_rank;
+
+WITH CTE2 AS (
+        SELECT
+        order_id,
+        product_id,
+        units,
+        DENSE_RANK() OVER(PARTITION BY order_id ORDER BY units DESC) AS prod_rank
+FROM    orders       
+ORDER BY order_id, prod_rank
+)
+SELECT  *
+FROM     CTE2
+WHERE   prod_rank = 2;
+
+-- Assgignment: Value Relative to a ROW
+-- How orders have changed overtime for each customer
+-- produce a table that contains info about each
+-- customer and their change in units form order to order
+
+
+SELECT * FROM orders;
+SELECT 
+        customer_id,
+        order_id,
+        SUM(units) as total_units,
+        LAG(units) OVER(PARTITION BY customer_id ORDER BY order_id) as prior_units
+FROM orders
+GROUP BY customer_id, order_id; 
--- SQL for Data Analysis Advanced SQL Querying Techniques/Section 6 Window Functions/Values within a Window.sql
+++ SQL for Data Analysis Advanced SQL Querying Techniques/Section 6 Window Functions/Values within a Window.sql
@@ -1,3 +1,4 @@
+-- Active: 1745290413437@@127.0.0.1@3306@maven_advanced_sql
 
 -- FIRST_VALUE, LAST_VALUE & NTH VALUE
 -- Creating a new table
@@ -15,4 +16,87 @@ VALUES
             ('Male', 'Noah', '95');
 
 -- View the table
-SELECT * from baby_names
+SELECT * from baby_names;
+
+-- Return the first name in each window
+SELECT 
+        gender,
+        name, 
+        babies,
+        FIRST_VALUE(name) OVER (PARTITION BY gender ORDER BY babies DESC) AS top_name
+FROM baby_names
+ORDER BY gender, babies DESC;
+
+-- Return the top name for each gender
+WITH top_nameCTE as(
+    SELECT 
+        gender,
+        name, 
+        babies,
+        FIRST_VALUE(name) OVER (PARTITION BY gender ORDER BY babies DESC) AS top_name
+    FROM baby_names)
+SELECT *
+FROM top_nameCTE
+WHERE name = top_name;
+
+-- Return the 2nd Name 
+SELECT 
+        gender,
+        name, 
+        babies,
+        NTH_VALUE(name, 2) OVER (PARTITION BY gender ORDER BY babies DESC) AS second_name
+    FROM baby_names;
+
+-- RETURN THE 2ND NAME by gender
+WITH top_nameCTE as(
+    SELECT 
+        gender,
+        name, 
+        babies,
+        NTH_VALUE(name, 2) OVER (PARTITION BY gender ORDER BY babies DESC) AS second_name
+    FROM baby_names)
+SELECT *
+FROM top_nameCTE
+WHERE name = second_name;
+
+-- Using ROW_NUMBER() TO GET THE 2ND MOST POPULAR NAME
+
+
+WITH top_nameCTE as(
+    SELECT 
+        gender,
+        name, 
+        babies,
+        ROW_NUMBER() OVER (PARTITION BY gender ORDER BY babies DESC) AS popularity
+    FROM baby_names)
+    SELECT * FROM top_nameCTE
+    WHERE popularity = 2;
+
+SELECT *
+FROM baby_girl_names;
+
+WITH 2PLACE AS (SELECT name, babies,
+ROW_NUMBER() OVER(PARTITION BY babies ORDER BY name) as bbcount
+FROM baby_girl_names)
+SELECT *
+FROM 2PLACE
+WHERE bbcount = 1 
+
+-- LEAD() AND LAG() 
+-- Return the difference between yearly scores
+WITH prior_yr_CTE as (
+        SELECT 
+                country, 
+                year, 
+                happiness_score,
+                LAG(happiness_score) OVER(PARTITION BY country ORDER BY year) as prior_yr_happiness_score
+                FROM happiness_scores
+)
+SELECT 
+        country,
+        year,
+        happiness_score,
+        prior_yr_happiness_score,
+        happiness_score - prior_yr_happiness_score as hs_change
+FROM    prior_yr_CTE;
+
