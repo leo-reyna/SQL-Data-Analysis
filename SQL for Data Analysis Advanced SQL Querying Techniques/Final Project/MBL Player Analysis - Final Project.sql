ef9c4f28ccaaf9df290d8f4ba8f646752c687218
LReyna <leo.reyna@gmail.com>
2026-01-18 18:46:25 -0500

    Updating final project tasks

--- SQL for Data Analysis Advanced SQL Querying Techniques/Final Project/MBL Player Analysis - Final Project.sql
+++ SQL for Data Analysis Advanced SQL Querying Techniques/Final Project/MBL Player Analysis - Final Project.sql
@@ -1,3 +1,4 @@
+-- Active: 1745290413437@@127.0.0.1@3306@sean_lahman_baseball
 -- SQL for Data Analysis - Advannced SQL Querying Technique
 
 -- PART I: SCHOOL ANALYSIS
@@ -12,29 +13,109 @@
 
 USE sean_lahman_baseball;
 
+-- 1. View the schools and school details tables
 SELECT * FROM schools;
-
 SELECT * FROM school_details;
+SELECT * from players;
 
--- 1. In each decade, how many schools were there that produced players?
+-- 2. In each decade, how many schools were there that produced players? [numeric functions]
+
+-- Solution from class:
 SELECT 
-    FLOOR(s.yearID / 10 ) * 10 as decade,
-    COUNT(DISTINCT s.schoolID) as number_of_schools
-FROM schools as s
-INNER JOIN school_details as dt
-    ON s.schoolID = dt.schoolID
+    ROUND(yearID, -1) as decade,  -- rounds the year to the nearest 10
+    COUNT(DISTINCT schoolID) AS num_schools
+FROM schools
 GROUP BY decade
 ORDER BY decade;
 
+-- My solution:
+SELECT 
+    CONCAT(FLOOR(yearID / 10) * 10, 's') AS decade, 
+    COUNT(DISTINCT schoolID) AS num_schools
+FROM schools
+GROUP BY decade
+ORDER BY decade;
 
--- 2. What are the names of the top 5 schools that produced the most players?
+-- 3. What are the names of the top 5 schools that produced the most players? [joints]
 
 SELECT 
-    dt.name_full AS university,
-    COUNT(DISTINCT s.playerID) AS player_count
+    sd.name_full as university,
+    COUNT(DISTINCT s.playerID) as num_players
 FROM schools AS s
-INNER JOIN school_details AS dt
-    ON s.schoolID = dt.schoolID
-GROUP BY dt.name_full
-ORDER BY player_count DESC
+LEFT JOIN school_details as sd
+    ON s.schoolID = sd.schoolID
+GROUP BY s.schoolID, sd.name_full
+ORDER BY num_players DESC
 LIMIT 5;
+
+
+-- 4. For each decade, what were the names of the top 3 schools that produced the most players?
+
+SELECT 
+        FLOOR(yearID / 10) * 10 AS decade, 
+        sd.name_full,
+        COUNT(DISTINCT s.playerID) AS player_count
+    FROM schools AS s
+    LEFT JOIN school_details AS sd
+        ON s.schoolID = sd.schoolID
+    GROUP BY decade, sd.schoolID, sd.name_full
+
+WITH ds AS (
+    SELECT 
+        FLOOR(yearID / 10) * 10 AS decade, 
+        sd.name_full,
+        COUNT(DISTINCT s.playerID) AS player_count
+    FROM schools AS s
+    LEFT JOIN school_details AS sd
+        ON s.schoolID = sd.schoolID
+    GROUP BY decade, sd.schoolID, sd.name_full
+),
+rn AS (
+    SELECT 
+        decade, 
+        name_full,
+        player_count,
+        ROW_NUMBER() OVER (
+            PARTITION BY decade 
+            ORDER BY player_count DESC
+        ) AS row_num
+    FROM ds
+)
+SELECT  
+    decade,
+    name_full,
+    player_count
+FROM rn
+WHERE row_num <= 3
+ORDER BY decade DESC, row_num;
+
+-- PART 2: Salary Analysis (window functions, rolling calculations, min/max filtering)
+-- Using the Sean Lahman Baseball Database - Complete the following steps:
+-- 1. Return the 20% of team in terms of average annual spending
+-- 2. For each team, show the cumulative sum of spending over the years
+-- 3. Return the first year that each team's cumulative spending surpasses 1 billion
+
+
+SELECT * FROM salaries;
+
+-- 1. Return the 20% of team in terms of average annual spending
+WITH total_spend_cte AS(
+            SELECT  
+                teamID, 
+                yearID, 
+                SUM(salary) as total_spend
+            FROM salaries
+            GROUP BY teamID, yearID
+            ORDER BY teamID, yearID
+),
+spent_perc_cte as (
+            SELECT
+                teamID, 
+                AVG(total_spend) as avg_spend,
+                NTILE(5) OVER(ORDER BY avg(total_spend) DESC) AS spend_perc
+            FROM total_spend_cte
+            GROUP BY teamID
+)
+SELECT teamID, round(avg_spend / 1000000, 1) as avg_spend_millions
+FROM spent_perc_cte
+WHERE spend_perc = 1;
